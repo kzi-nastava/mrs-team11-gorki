1433165421d8842f74e870a9f7927dbc4529c565
MarkanP <marko.pavlovic2404004@gmail.com>
2025-12-21 05:01:03 +0100

    Implement navbar and add background color (closes #1)

--- frontend/Gorki/src/app/app.routes.ts
+++ frontend/Gorki/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'frontend\Gorki\src\app\layout\navbar',
+        component: Navbar
+    },
+];
