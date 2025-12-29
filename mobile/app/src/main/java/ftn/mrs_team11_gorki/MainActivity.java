41ce0e27ae7e9fdb74c726efc9bc13ce70d7cc6b
ognjenmiletic04 <167654053+ognjenmiletic04@users.noreply.github.com>
2025-12-29 05:53:37 +0100

    Feat: Add initial home page and nav bar (closes #26)

---
+++ mobile/app/src/main/java/ftn/mrs_team11_gorki/MainActivity.java
@@ -0,0 +1,37 @@
+package ftn.mrs_team11_gorki;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // --- TOOLBAR ---
+        Toolbar toolbar = findViewById(R.id.navbar);  // instancira Toolbar preko ID-ja
+        setSupportActionBar(toolbar);                 // postavlja ga kao ActionBar
+
+        // --- EDGE TO EDGE padding ---
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    //Button register = findViewById(R.id.register);
+   // Button login = findViewById(R.id.login);
+}
