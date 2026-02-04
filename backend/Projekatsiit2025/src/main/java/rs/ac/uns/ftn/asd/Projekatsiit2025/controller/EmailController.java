b10dd4215d845a3e37304398d3f6a99d59acdfc1
Luka Berić <beric.sv82.2024@uns.ac.rs>
2026-02-05 00:24:43 +0100

    Feat: Implemented backend service for sending emails with SMTP ( Closes #99 )

---
+++ backend/Projekatsiit2025/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2025/controller/EmailController.java
@@ -0,0 +1,34 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2025.controller;
+
+import rs.ac.uns.ftn.asd.Projekatsiit2025.model.EmailDetails;
+import rs.ac.uns.ftn.asd.Projekatsiit2025.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired private EmailService emailService;
+
+    @PostMapping("/sendMail")
+    public String
+    sendMail(@RequestBody EmailDetails details)
+    {
+        String status
+            = emailService.sendSimpleMail(details);
+
+        return status;
+    }
+
+    @PostMapping("/sendMailWithAttachment")
+    public String sendMailWithAttachment(
+        @RequestBody EmailDetails details)
+    {
+        String status
+            = emailService.sendMailWithAttachment(details);
+
+        return status;
+    }
+}
