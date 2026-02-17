c8ea1417eb0d8d89edc4e385b3a7d9fdf2745690
ognjenmiletic04 <167654053+ognjenmiletic04@users.noreply.github.com>
2026-02-17 18:38:47 +0100

    Implement map using OSMDroid library and show all vehicles on map (closes #145)

--- mobile/app/src/main/java/ftn/mrs_team11_gorki/auth/ApiClient.java
+++ mobile/app/src/main/java/ftn/mrs_team11_gorki/auth/ApiClient.java
@@ -7,22 +7,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import ftn.mrs_team11_gorki.auth.TokenStorage;
 public class ApiClient {
 
-    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
+    private static Retrofit retrofitAuth;
+    private static Retrofit retrofitPublic;
 
     public static Retrofit getRetrofit(Context context) {
-        if (retrofit == null) {
+        if (retrofitAuth == null) {
             TokenStorage tokenStorage = new TokenStorage(context.getApplicationContext());
 
             OkHttpClient okHttp = new OkHttpClient.Builder()
                     .addInterceptor(new AuthInterceptor(tokenStorage))
                     .build();
 
-            retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:8080/")
+            retrofitAuth = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
                     .client(okHttp)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
-        return retrofit;
+        return retrofitAuth;
+    }
+
+    public static Retrofit getPublicRetrofit() {
+        if (retrofitPublic == null) {
+            OkHttpClient okHttp = new OkHttpClient.Builder().build();
+
+            retrofitPublic = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttp)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofitPublic;
+    }
+
+
+    public static void reset() {
+        retrofitAuth = null;
+        retrofitPublic = null;
     }
 }
