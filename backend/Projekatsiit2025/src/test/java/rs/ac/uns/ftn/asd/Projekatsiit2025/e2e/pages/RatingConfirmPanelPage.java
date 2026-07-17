dc0331f2d7855886e38897e51c025d48e24958ce
Luka Berić <beric.sv82.2024@uns.ac.rs>
2026-07-17 14:03:12 +0200

    Removed unused import and depricated method

--- backend/Projekatsiit2025/src/test/java/rs/ac/uns/ftn/asd/Projekatsiit2025/e2e/pages/RatingConfirmPanelPage.java
+++ backend/Projekatsiit2025/src/test/java/rs/ac/uns/ftn/asd/Projekatsiit2025/e2e/pages/RatingConfirmPanelPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class RatingConfirmPanelPage {
 
     private final WebDriver driver;
